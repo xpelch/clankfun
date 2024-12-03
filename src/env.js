758e50b47c6ad06447132b7f2259853dadcdb31f
Nick Tikhonov <ndtikhonov@gmail.com>
2024-12-03 15:32:33 +0100

    Index supply

--- src/env.js
+++ src/env.js
@@ -16,6 +16,7 @@ export const env = createEnv({
     DATABASE_URL: z.string(),
     CRON_SECRET: z.string(),
     REDIS_URL: z.string(),
+    INDEX_SUPPLY_API: z.string(),
   },
 
   /**
@@ -42,6 +43,7 @@ export const env = createEnv({
     DATABASE_URL: process.env.DATABASE_URL,
     CRON_SECRET: process.env.CRON_SECRET,
     REDIS_URL: process.env.REDIS_URL,
+    INDEX_SUPPLY_API: process.env.INDEX_SUPPLY_API,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
